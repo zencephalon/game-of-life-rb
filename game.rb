692845e5879631ad153068f962eee72fb54e9cfb
Matthew Bunday <zenceph@gmail.com>
2015-02-05 10:04:04 -0500

    Implement game logic.

--- game.rb
+++ game.rb
@@ -2,9 +2,41 @@ class Life
   attr_accessor :board
 
   def initialize(board_str)
-    @board = board_str.split(',').map do |row_str|
+    rows = board_str.split(',')
+    @height = rows.size
+    @width = rows[0].size
+    @board = rows.map do |row_str|
       row_str.split('').map do |char|
         char == 'X'
+      end
+    end
+  end
+
+  def cell(y, x)
+    if y < 0 || x < 0 || x >= @width || y >= @height
+      return false
+    end
+    @board[y][x]
+  end
+
+  def neighbors(y, x)
+    num_neighbors = 0
+    (-1..1).each do |dy|
+      (-1..1).each do |dx|
+        if cell(y + dy, x + dx) && !(dy == 0 && dx == 0)
+          num_neighbors += 1
+        end
+      end
+    end
+    num_neighbors
+  end
+
+  def next
+    @board = (0...@height).map do |y|
+      (0...@width).map do |x|
+        n = neighbors(y, x)
+        puts "At #{y},#{x} neighbors: #{n}"
+        cell(y, x) ? (n == 2 || n == 3) : n == 3
       end
     end
   end
@@ -19,4 +51,4 @@ class Life
 end
 
 
-#l = Life.new("....,.XX.,X..X")
+#l = Life.new("....,.XXX,....")
